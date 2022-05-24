bca97291ebd7962df04d7dd8289f3d62caeb8b40
Melanija <melanija.sibakovska1@gmail.com>
2022-05-24 12:06:45 +0300

    new

---
+++ src/Components/REACT_BASE/Propsas.jsx
@@ -0,0 +1,11 @@
+function Propsas({text}) {
+    return <h1 style = {
+        {
+            backgroundColor: text,
+            fontSize: '23px',
+            padding: '20px',
+            width: '450px'
+        }
+    }>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus enim error consequuntur quam eius odio ut possimus voluptas animi?</h1>
+}
+export default Propsas;
---
+++ src/Components/REACT_BASE/ZebraiIrBebrai.jsx
@@ -0,0 +1,8 @@
+function ZebraiIrBebrai() {
+    return <h1 style = {
+        {
+            color: this.state.color
+        }
+    }>Lorem ipsum dolor sit amet consectetur adipisicing elit?</h1>
+}
+export default ZebraiIrBebrai;
