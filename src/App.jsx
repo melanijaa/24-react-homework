28ffb8d79662a02d2977f555be0e6c66f10e37bf
Melanija <melanija.sibakovska1@gmail.com>
2022-05-26 12:47:30 +0300

    homework

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,113 @@
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
-import Zuikis from './Components/REACT_BASE/Zuikis';
-import Propsas from './Components/REACT_BASE/Propsas';
-import ZebraiIrBebrai from './Components/REACT_BASE/ZebraiIrBebrai';
+import rand from './Functions/rand';
+
+const cats = [
+    'Mulkis',
+    'Kakius',
+    'Pilkius',
+    'Balčius'
+];
+
+const dogs = [
+    'Sniego',
+    'Dingo',
+    'Atsirado',
+    'Pifas',
+    'Bobikas'
+];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <Zuikis spalva = 'pink'></Zuikis>
-      <Propsas text = 'green'></Propsas>
-      <ZebraiIrBebrai></ZebraiIrBebrai>
-      <button onClick={() => this.setState({ color: "blue" })}>Blue</button>
-      <button onClick={() => this.setState({ color: "red" })}>Red</button>
-      </header>
-    </div>
-  );
+
+    const [kv, setKv] = useState(null);
+    const istorija = useRef([]);
+
+    const [katinai, setKatinai] = useState([]);
+
+    // PIRMAS KROVIMAS
+    useEffect(() => {
+        setKv(JSON.parse(localStorage.getItem('kv'))); // gali buti null arba []
+    }, []);
+
+    // UZSAUGOS POKYCIUS
+    useEffect(() => {
+        if (null === kv) {
+            return;
+        }
+        localStorage.setItem('kv', JSON.stringify(kv));
+        istorija.current.unshift(kv);
+    }, [kv]);
+
+    const prideti = () => {
+        const kiekis = rand(5, 10);
+        const kvadratukai = [];
+        for (let i = 0; i < kiekis; i++) {
+            kvadratukai.push('^o^');
+        }
+        setKv(k => null === k ? [...kvadratukai] : [...k, ...kvadratukai]);
+    }
+
+    const isvalyti = () => {
+        setKv([]);
+    }
+
+    const atgal = () => {
+        let senas = istorija.current.shift();
+        if (!senas) {
+            setKv([]);
+        } else if (senas.length === kv.length) {
+            senas = istorija.current.shift();
+            if (!senas) {
+                setKv([]);
+            } else {
+                setKv(senas);
+            }
+        }
+        else {
+            setKv(senas);
+        }
+    }
+
+    const addCats = () => {
+        setKatinai(x => [...x, ...cats]);
+    }
+    const addDogs = () => {
+        setKatinai(dogs);
+    }
+    const clear = () => {
+        setKatinai([]);
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>PRAKTIMUMAS</h1>
+                <div className="kvc">
+                    {
+                        kv ? kv.map((k, i) => <div key={i} className="kv">{k}</div>) : null
+                    }
+                </div>
+
+                <button onClick={prideti}>Pridėti</button>
+                <button onClick={isvalyti}>Išvalyti</button>
+                <button onClick={atgal}>Atgal</button>
+                <div className="green-field">
+
+                    {
+                        katinai.map((c, i) => <div key={i}>{c}</div>)
+                    }
+
+                    <div className="buttons-field">
+
+                        <button onClick={addCats}>Katinukai</button>
+                        <button onClick={addDogs}>Šuniai</button>
+                        <button className="red" onClick={clear}>Namo visi</button>
+                    </div>
+
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
