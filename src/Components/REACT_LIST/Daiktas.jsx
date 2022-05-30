ede544c58e5200e94fc9a789ce34b6432a5f516b
Melanija <melanija.sibakovska1@gmail.com>
2022-05-30 12:05:17 +0300

    hw

--- src/Components/REACT_LIST/Daiktas.jsx
+++ src/Components/REACT_LIST/Daiktas.jsx
@@ -1,11 +1,6 @@
 function Daiktas({Things}) {
     <div>
-        <h2>
-            Id: {Things.id},
-            Type: {Things.type},
-            Name: {Things.name},
-            Color: {Things.color}
-        </h2>
+        <h2>Id: {Things.id} Type: {Things.type} Name: {Things.name} Color: {Things.color}</h2>
     </div>
 };
 
--- src/Components/REACT_LIST/Tvenkinys.jsx
+++ src/Components/REACT_LIST/Tvenkinys.jsx
@@ -5,13 +5,16 @@ function Tvenkinys({seaPlaners}) {
     return (
         <>
         {
-            seaPlaners.map(things => things.id % 2 ? <Daiktas key={things.id} Things={things}></Daiktas> : null)
+            seaPlaners.map(things => things.id % 2 ? <Daiktas key={things.id}  Things={things} ></Daiktas> : null)
         }
+        <br/>
         {
-            seaPlaners.map(things => things.id % 2 ? null : <Daiktas key={things.id} Things={things}></Daiktas>)
+            seaPlaners.map(things => things.id % 2 ? null : <Daiktas key={things.id}  Things={things}  ></Daiktas>)
         }
         </>
     )
 };
 
 export default Tvenkinys;
+//
+//       
