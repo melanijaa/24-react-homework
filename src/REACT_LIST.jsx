1705f5f8afcb6f02be11e6dd9e80232b3abc95b4
Melanija <melanija.sibakovska1@gmail.com>
2022-05-31 09:37:51 +0300

    new app.jsx

--- src/REACT_LIST.jsx
+++ src/REACT_LIST.jsx
@@ -1,11 +1,13 @@
 import './App.css';
+import Bala from './Components/REACT_LIST/Bala';
+//import Tvenkinys from './Components/REACT_LIST/Tvenkinys';
 
 function App() {
 
     return (
         <div className="App">
             <header className="App-header">
-                
+                <Bala></Bala>
             </header>
         </div>
     );
